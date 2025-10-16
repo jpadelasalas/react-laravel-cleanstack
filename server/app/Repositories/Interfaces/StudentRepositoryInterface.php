d72a14fdc376d5c61468019a75868e04079a9cb2
John Patrick <jpadelasalas@gmail.com>
2025-10-16 16:09:01 +0800

    Update StudentRepositoryInterface.php

--- server/app/Repositories/Interfaces/StudentRepositoryInterface.php
+++ server/app/Repositories/Interfaces/StudentRepositoryInterface.php
@@ -11,10 +11,4 @@ interface StudentRepositoryInterface
     public function update($id, array $data);
     public function updateOrCreate(array $columnCondition, array $data);
     public function delete($id);
-
-    // Relationships
-    public function enrollInCourses($studentId, array $courseIds);
-    public function removeFromCourses($studentId, array $courseIds);
-    public function syncCourses($studentId, array $courseIds);
-    public function getCourses($studentId);
 }

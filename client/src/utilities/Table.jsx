5fba40fa7991cc76f23f40d40814724113740922
John Patrick <jpadelasalas@gmail.com>
2025-10-21 20:36:36 +0800

    Update Table.jsx

--- client/src/utilities/Table.jsx
+++ client/src/utilities/Table.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Table = React.memo(({ tHead, children }) => {
   return (
     <table className="w-full text-sm text-left">
-      <thead className="text-xs uppercase bg-[#B2CAFF] dark:bg-gray-700">
+      <thead className="text-xs uppercase bg-[#B2CAFF] dark:bg-gray-700 text-black dark:text-white">
         <tr>
           {tHead.map((name, index) => (
             <th scope="col" key={`${index}-${name}`} className="px-6 py-3">
